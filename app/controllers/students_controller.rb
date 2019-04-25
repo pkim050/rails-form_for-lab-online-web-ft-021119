88f1fa278c5a8ee14cf17e06642a6aa6234843c5
Patrick Kim <pkim050@gmail.com>
2019-04-25 13:19:58 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,26 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new(first_name: params[:first_name], last_name: params[:last_name])
+    @student.save
+    redirect_to @student
   end
 
   def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student).permit(:first_name, :last_name))
+    redirect_to @student
   end
 
   def edit
@@ -19,4 +28,7 @@ class StudentsController < ApplicationController
 
   private
 
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end
 end
