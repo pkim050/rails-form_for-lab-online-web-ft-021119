79132a58eefa189111c92b0382991e1b4d8c8405
Patrick Kim <pkim050@gmail.com>
2019-04-25 13:37:06 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(student_params(:first_name, :last_name)
+    @student = Student.new(student_params(:first_name, :last_name))
     @student.save
     redirect_to @student
   end
