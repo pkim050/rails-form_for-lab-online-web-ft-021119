7bca1791a2c3fb59957f078bd2a60269ac81f91c
Patrick Kim <pkim050@gmail.com>
2019-04-25 13:34:14 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,17 +1,26 @@
 class SchoolClassesController < ApplicationController
   def index
+    @schoolclasses = SchoolClass.all
   end
 
   def show
+    @schoolclass = SchoolClass.find(params[:id])
   end
 
   def new
+    @schoolclass = SchoolClass.new
   end
 
   def create
+    @schoolclass = SchoolClass.new(school_class_params(:title, :room_number))
+    @schoolclass.save
+    redirect_to @schoolclass
   end
 
   def update
+    @schoolclass = SchoolClass.find(params[:id])
+    @schoolclass.update(school_class_params(:room_number))
+    redirect_to @schoolclass
   end
 
   def edit
@@ -19,4 +28,7 @@ class SchoolClassesController < ApplicationController
 
   private
 
+  def school_class_params(*args)
+    params.require(:school_student).permit(*args)
+  end
 end
