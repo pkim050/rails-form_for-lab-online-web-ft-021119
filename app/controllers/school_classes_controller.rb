818900220a5ebb06ed75c41bfd106f575c60b0f2
Patrick Kim <pkim050@gmail.com>
2019-04-25 14:53:15 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -19,7 +19,7 @@ class SchoolClassesController < ApplicationController
 
   def update
     @schoolclass = SchoolClass.find(params[:id])
-    @schoolclass.update(school_class_params(:room_number))
+    @schoolclass.update(school_class_params(:title, :room_number))
     redirect_to @schoolclass
   end
 
